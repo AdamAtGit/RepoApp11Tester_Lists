46ce359e11e583b2f996903391a3ddea36921647
AdamAtGit <43450212+AdamAtGit@users.noreply.github.com>
2018-09-25 16:05:57 -0400

    initial ADD

--- App11Tester_Lists/MainPage.xaml.cs
+++ App11Tester_Lists/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +26,52 @@ namespace App11Tester_Lists
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //Works for a string
+        ObservableCollection<string> myCollection = new ObservableCollection<string>();
+        public ObservableCollection<string> MyCollection
+        { get { return myCollection; } }
+
+        //for a Class/Model
+
+        ObservableCollection<string> contentCollection = new ObservableCollection<string>();
+        public ObservableCollection<string> contentCollection
+        { get { return contentCollection; } }
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private int i = 0;
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            i++;
+            myCollection.Add(i.ToString());
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            //Replace string with your object type
+            while (myListBox.SelectedItems.Count > 0)
+            {
+                myCollection.Remove((string)myListBox.SelectedItem);
+            }
+        }
+
+        private void btnClassAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClassRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public class Content
+        {
+
+
         }
     }
 }
